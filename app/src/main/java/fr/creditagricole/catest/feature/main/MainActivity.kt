226b5b2d963a7bd3710d84f664a01e374d059659
Antoine Lapeyre <antoinelf31@gmail.com>
2024-03-03 11:31:15 +0100

    Initial commit

---
+++ app/src/main/java/fr/creditagricole/catest/feature/main/MainActivity.kt
@@ -0,0 +1,38 @@
+package fr.creditagricole.catest.feature.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import fr.creditagricole.catest.ui.theme.CATestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CATestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CATestTheme {
+        
+    }
+}
